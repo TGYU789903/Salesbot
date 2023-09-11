0b2d31520b21ca811cdaddd84118b13062705387
AashiKhatri <132686782+AashiKhatri@users.noreply.github.com>
2023-09-11 13:18:15 +0530

    Update langchaincode.py

--- langchaincode.py
+++ langchaincode.py
@@ -20,6 +20,11 @@ chain = RetrievalQA.from_chain_type(llm=OpenAI(), chain_type="stuff", retriever=
 
 chat_history = []
 
+# Start with the bot's opening line
+opening_line = "Hello, I'm Jacob from AryanTech Company. I'm calling you regarding our service to assist you in securing a job. Are you looking for any job opportunities right now?"
+chat_history.append(opening_line)
+print(opening_line)
+
 while True:
     # Ask user for a query
     query = input("Please enter your query (or type 'exit' to end): ")
@@ -44,6 +49,35 @@ while True:
 with open('chat_history.txt', 'w') as f:
     for chat in chat_history:
         f.write("%s\n" % chat)
+# ________________________________________________
+# chat_history = []
+
+# while True:
+#     # Ask user for a query
+#     query = input("Please enter your query (or type 'exit' to end): ")
+
+#     # Exit loop if user types 'exit'
+#     if query.lower() == 'exit':
+#         break
+    
+#     # If you want to provide the entire chat history as context
+#     context = ". ".join(chat_history)
+#     full_query = context + ". " + query if context else query
+
+#     response = chain({"question": full_query})
+
+#     # Save user's query and model's response to chat_history
+#     chat_history.append(query)
+#     chat_history.append(response['result'])
+
+#     print(response['result'])
+
+# # If you wish to save the chat history to a file
+# with open('chat_history.txt', 'w') as f:
+#     for chat in chat_history:
+#         f.write("%s\n" % chat)
+
+# _______________________________________________________________________________
 # #query = "what is your name"
 # query = "what payment options are available?"
 # response = chain({"question": query})
